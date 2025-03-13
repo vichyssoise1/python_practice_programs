52173062b456a3126103f5b85239e125cd7c0680
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 03:42:11 +0800

    added modulous operator and print function to show the result

--- programs_batch_two/print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
+++ programs_batch_two/print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
@@ -1,3 +1,5 @@
 #ask user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+#add modulous operator to find the remainder and print it
+print(num1 % num2)
