c86193293aab3cdb4fc0755fccd0a5c059364baf
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 02:15:51 +0800

    add logic to add the two numbers and then print the sum

--- programs_batch_one/sum_of_two_numbers.py
+++ programs_batch_one/sum_of_two_numbers.py
@@ -1,3 +1,5 @@
 #user intput where the program will ask the user for a number
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
+#logic to add the two numbers and output the sum
+print(num1 + num2)
