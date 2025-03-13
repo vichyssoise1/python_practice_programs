e1c2a1eebe01e3ccfce25589734018e1660f8670
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 03:24:20 +0800

    added the comparison logic and print to display the output

--- programs_batch_two/ask_user_two_numbers_then_print_the_smaller_number.py
+++ programs_batch_two/ask_user_two_numbers_then_print_the_smaller_number.py
@@ -1,3 +1,5 @@
 #ask the user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+#add comparison logic and print the smaller number
+print(min(num1, num2))
