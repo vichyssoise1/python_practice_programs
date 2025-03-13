e2d9811919fd14f8edad4563594244587a2fd509
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 04:45:57 +0800

    added print function to display the output

--- programs_batch_three/ask_ten_numbers_print_unique_numbers.py
+++ programs_batch_three/ask_ten_numbers_print_unique_numbers.py
@@ -2,3 +2,5 @@
 numbers = [int(input("Enter a number: ")) for _ in range(10)]
 #add logic to filter out unique numbers from the list
 unique_numbers = [num for num in numbers if numbers.count(num) == 1]
+#add print statement to display unique numbers
+print(unique_numbers)
