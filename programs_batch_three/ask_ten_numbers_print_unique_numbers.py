b4144e5ffe7c4522aa5be2ec151ba0b0e409caa3
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 04:45:12 +0800

    added logic to filter out the unique numbers from the list

--- programs_batch_three/ask_ten_numbers_print_unique_numbers.py
+++ programs_batch_three/ask_ten_numbers_print_unique_numbers.py
@@ -1,2 +1,4 @@
 #add list to collect numbers from user input asking 10 times
 numbers = [int(input("Enter a number: ")) for _ in range(10)]
+#add logic to filter out unique numbers from the list
+unique_numbers = [num for num in numbers if numbers.count(num) == 1]
