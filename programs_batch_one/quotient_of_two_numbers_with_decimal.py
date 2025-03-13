5ec486b73e43523b59786285f6fd5124fa83e7a6
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 02:22:59 +0800

    added computing logic for quotient with decimal and printing the output

--- programs_batch_one/quotient_of_two_numbers_with_decimal.py
+++ programs_batch_one/quotient_of_two_numbers_with_decimal.py
@@ -1,3 +1,5 @@
 #user input for the 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+#computing the quotient of the 2 numbers and displaying the result
+print(num1 / num2)
