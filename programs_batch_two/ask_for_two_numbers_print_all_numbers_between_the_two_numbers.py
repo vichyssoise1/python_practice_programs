21d64201e1daec87d1d21cbc7a864e2b9c6c1bc5
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 04:14:28 +0800

    added if statement to check if first number is greater than the second number

--- programs_batch_two/ask_for_two_numbers_print_all_numbers_between_the_two_numbers.py
+++ programs_batch_two/ask_for_two_numbers_print_all_numbers_between_the_two_numbers.py
@@ -1,3 +1,7 @@
 #ask user input for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+#check if the first number is greater than the second number
+if num1 > num2:
+    #if the first number is greater than the second number, swap the two numbers
+    num1, num2 = num2, num1
