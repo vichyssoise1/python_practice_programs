910ccafebf4a20a65660b7b4fa5d5e0b89cd217b
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 02:19:42 +0800

    added computing logic and print the output

--- programs_batch_one/product_of_the_two_numbers.py
+++ programs_batch_one/product_of_the_two_numbers.py
@@ -1,3 +1,5 @@
 #user intput for the two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+#computing the product of the two numbers and printing the result
+print(num1*num2)
