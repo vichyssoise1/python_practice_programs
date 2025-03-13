a0442f0bd62e5c5d7334ada6e821e4704852537a
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 03:34:37 +0800

    added floor division operator and print function

--- programs_batch_two/quotient_of_two_numbers_without_decimal.py
+++ programs_batch_two/quotient_of_two_numbers_without_decimal.py
@@ -1,3 +1,5 @@
 #ask for two numbers
 numb1 = int(input("Enter the first number: "))
 numb2 = int(input("Enter the second number: "))
+#divide the two numbers using floor division and print the result
+print(numb1 // numb2)
