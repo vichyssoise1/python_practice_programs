85c4818880ad39aa891ce5f6502d35f3b504333d
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 04:56:21 +0800

    added logic to remove duplicate numbers after the first occurence

--- programs_batch_three/display_numbers_first_occurrence.py
+++ programs_batch_three/display_numbers_first_occurrence.py
@@ -1,2 +1,8 @@
 #ask for ten numbers
 numbers = [int(input("Enter a number: ")) for _ in range(10)]
+unique_numbers =[]
+# Create a new list that contains only the first occurrence of each number
+for num in numbers:
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+        
