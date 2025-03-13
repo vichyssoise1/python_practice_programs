3985a5e6673f429fc587183d77a6b8fc7da25ac0
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 03:28:07 +0800

    added comparison logic and the print function to show the output

--- programs_batch_two/print_not_equal_if_numbers_are_not_eqaul.py
+++ programs_batch_two/print_not_equal_if_numbers_are_not_eqaul.py
@@ -1,3 +1,8 @@
 #ask the user for two numbers 
 number_1 = int(input("Enter the first number: "))
 number_2 = int(input("Enter the second number: "))
+
+#add comparison logic to check if the numbers are not equal
+if number_1 != number_2:
+    #print the output
+    print("Not Euqal")
