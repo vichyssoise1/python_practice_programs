443794356ee075c0651f39e9bc93634e94d05342
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 05:07:34 +0800

    store unique numbers using a set

--- programs_batch_three/print_duplicate_or_unique.py
+++ programs_batch_three/print_duplicate_or_unique.py
@@ -1,9 +1,11 @@
 #initialize empty set
 numbers = set()
-
+#infinitely take input from user
 while True:
     try:
         num = int(input("Enter a number: "))
+        numbers.add(num) #add number to set
     except ValueError:
         break
+    
     
