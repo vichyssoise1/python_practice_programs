2724c5180939b8cc8cb5ef7efad273b5c084cf4d
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 03:49:33 +0800

    added the minus operator to calculate and print function to show the result

--- programs_batch_two/ask_ten_numbers_then_first_minus_remaining_numbers.py
+++ programs_batch_two/ask_ten_numbers_then_first_minus_remaining_numbers.py
@@ -1,2 +1,4 @@
 #add user input to the list and for loop to ask 10 times
 a, *rest = [int(input("Enter a number: ")) for _ in range(10)]
+#subtract the remaining numbers from the first number
+print(a - sum(rest))
