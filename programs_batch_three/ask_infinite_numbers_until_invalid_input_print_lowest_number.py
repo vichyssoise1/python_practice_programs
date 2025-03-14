adb9109d73528c778d803973c247bb7be01c1c71
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 17:14:51 +0800

    added intialization and logic to track the lowest number from the user input

--- programs_batch_three/ask_infinite_numbers_until_invalid_input_print_lowest_number.py
+++ programs_batch_three/ask_infinite_numbers_until_invalid_input_print_lowest_number.py
@@ -1,6 +1,12 @@
+#initalize the variable
+lowest = None
+
 #loop infinitely until the user inputs an invalid number
 while True:
     try:
         num = int(input("Enter a number: "))
     except ValueError:
         break
+    
+if lowest is None or num < lowest:
+    lowest = num
