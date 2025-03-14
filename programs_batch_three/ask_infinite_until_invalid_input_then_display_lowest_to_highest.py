6c786fcf994624ab4dab81ed9a9063c7c84e5a88
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 20:27:47 +0800

    renamed file

---
+++ programs_batch_three/ask_infinite_until_invalid_input_then_display_lowest_to_highest.py
@@ -0,0 +1,14 @@
+#initialize empty varaible list
+numbers = []
+
+# Continuously ask for user input until an invalid entry is given
+while True:
+    try:
+        #Ask for user input
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
+numbers.sort()
+print(numbers)
