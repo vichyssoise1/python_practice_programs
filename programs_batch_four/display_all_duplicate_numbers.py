37b5538d4a50f1f6920d1c9580d258299dc97a7c
vichyssoise <carlosbondocdizon@gmail.com>
2025-03-14 18:39:25 +0800

    added logic to identify numbers that have duplicates

--- programs_batch_four/display_all_duplicate_numbers.py
+++ programs_batch_four/display_all_duplicate_numbers.py
@@ -1,5 +1,12 @@
 #ask user input for a number
 numbers = int(input("Enter a number: "))
 
+#empty list to store duplicates
+duplicates = []
+#loop to check for duplicates
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
 # Print the numbers to verify input collection.
-print("Numbers entered:", numbers)
+print("Numbers entered:", numbers)  
